d5856cf8b1585585ad5ebec9976d2f3b5723618a
TetianaAsadova <114417194+TetianaAsadova@users.noreply.github.com>
2023-03-15 12:56:40 +0200

    sum1

---
+++ src/js/02-lightbox.js
@@ -0,0 +1,26 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+galleryRef.innerHTML = createMarkup(galleryItems);
+
+function createMarkup(items) {
+    return items.map(({ preview, original, description }) => {
+        return `
+            <a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+            `;
+        })
+        .join('');
+}
+
+galleryRef.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (e.target.nodeName !== "IMG") {
+        return;
+    }
+    var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, });
+    
+});
