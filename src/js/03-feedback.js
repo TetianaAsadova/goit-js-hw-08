adaa6dace732c4da5e97e635ba7cf0b451222e1d
TetianaAsadova <114417194+TetianaAsadova@users.noreply.github.com>
2023-03-17 00:25:03 +0200

    sum-8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,9 +1,42 @@
-import throttle from 'lodash.throttle';
+import throttle from "lodash.throttle";
+import '../css/common.css';
+import '../css/03-feedback.css';
 
-const feedbackFormRef = document.querySelector('feedback-form');
+const formRef = document.querySelector('.feedback-form');
+const textareaRef = document.querySelector('.feedback-form textarea');
+const emailRef = document.querySelector('.feedback-form input');
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {
+    email: '',
+    message: '',
+}
 
-feedbackFormRef.addEventListener(input, (e) => {
+formRef.addEventListener('submit', onFormSubmit);
+formRef.addEventListener('input', throttle(onTextareaInput, 500));
+
+// formRef.addEventListener('input', e => {
+//     console.log(`e.target.name`, e.target.name);
+//     console.log(`e.target.value`, e.target.value);
+// })
+
+addTextarea();
+
+function onFormSubmit(e) {
     e.preventDefault();
-    localStorage.setItem('email', email);
-    localStorage.setItem('message', message);
-});
+    e.target.reset();
+    localStorage.removeItem(STORAGE_KEY);
+};
+
+function onTextareaInput(e) {
+    formData[e.target.name] = e.target.value;
+    const formDataJson = JSON.stringify(formData);
+    localStorage.setItem(STORAGE_KEY, formDataJson);
+};
+
+function addTextarea(e) {
+    const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (savedMessage) {
+        textareaRef.value = savedMessage.message;
+        emailRef.value = savedMessage.email;
+    }
+}
